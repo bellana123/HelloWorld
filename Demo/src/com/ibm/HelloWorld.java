94a888010f1a04e3853d57354d081fd82959ace7
VINOD KUMAR <vinodmarisarla@gmail.com>
2017-02-22 22:45:30 +0530

    commit code

---
+++ Demo/src/com/ibm/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.ibm;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world ******************");
+
+	}
+
+}
